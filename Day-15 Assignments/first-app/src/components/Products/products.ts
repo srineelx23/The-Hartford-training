8d7547222a82b92b18da0ad95996e12deaf3fcb4
srineelx23 <srineelx23@gmail.com>
2026-01-20 15:15:31 +0530

    Fix nested git repo and add assignments properly

---
+++ Day-15 Assignments/first-app/src/components/Products/products.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  imports: [],
+  templateUrl: './products.html',
+  styleUrl: './products.css',
+})
+export class Products {
+}
