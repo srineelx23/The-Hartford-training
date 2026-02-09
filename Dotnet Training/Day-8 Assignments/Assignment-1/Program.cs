830d31015a3ae03208e1598e12f70e27d175a517
srineelx23 <srineelx23@gmail.com>
2026-02-09 21:33:55 +0530

    Day-8 Assignments

---
+++ Dotnet Training/Day-8 Assignments/Assignment-1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Assignment_1
+{
+    delegate void Print();
+    class Money
+    {
+        protected uint note;
+        protected uint coin;
+        public Money(uint n, uint c)
+        {
+            this.note = n;
+            this.coin = c;
+        }
+    }
+
+    class Rupee : Money
+    {
+        public Rupee(uint rupees, uint paise) : base(rupees, paise) { }
+        public void Display()
+        {
+            Console.WriteLine("Rs. {0}.{1}", note, coin);
+        }
+    }
+
+    class Dollar : Money
+    {
+        public Dollar(uint dollar, uint cent) : base(dollar, cent) { }
+        public void Info()
+        {
+            Console.WriteLine("${0}.{1}", note, coin);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rupee m1 = new Rupee(1000, 55);
+            Dollar m2 = new Dollar(100, 75);
+            Print GP = m1.Display;
+            GP();
+            GP = m2.Info;
+            GP();
+        }
+    }
+}
